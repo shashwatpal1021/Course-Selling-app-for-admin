162e516daa49c3764478869e70d80424a15e752a
shashwat pal <shashwatpal1021@gmail.com>
2024-03-03 17:24:04 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const adminRouter = require("./routes/admin");
+const userRouter = require("./routes/user");
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.get("/", (req, res) => {
+    res.send("hello world")
+})
+
+app.use("/admin", adminRouter)
+app.use("/user", userRouter)
+
+
+// Connect to MongoDB
+// DONT MISUSE THIS THANKYOU!!
+
+
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect('mongodb+srv://shashwat:shashwat@cluster0.raimqqs.mongodb.net/courses')
+        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log("MONGODB connection FAILED ", error);
+        process.exit(1)
+    }
+}
+
+connectDB()
+    .then(() => {
+        app.listen(3000 || 8000, () => {
+            console.log(`⚙️ Server is running at port : ${3000}`);
+        })
+    })
+    .catch((err) => {
+        console.log("MONGO db connection failed !!! ", err);
+    })
---
+++ frontend/src/components/AddCourse.jsx
@@ -0,0 +1,82 @@
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import {Card, colors} from "@mui/material";
+import {useState} from "react";
+import axios from "axios";
+import { BASE_URL } from "../config.js"
+import { useNavigate } from "react-router-dom";
+function AddCourse() {
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [image, setImage] = useState("");
+    const [price, setPrice] = useState(0)
+    const navigate=useNavigate();
+
+    return <div style={{display: "flex", justifyContent: "center", minHeight: "80vh", flexDirection: "column", backgroundColor:"grey"}}>
+        <div style={{display: "flex", justifyContent: "center"}}>
+            <Card varint={"outlined"} style={{width: 400, padding: 20, marginTop: 30, height: "100%"}}>
+                <TextField
+                    style={{marginBottom: 10}}
+                    onChange={(e) => {
+                        setTitle(e.target.value)
+                    }}
+                    fullWidth={true}
+                    label="Title"
+                    variant="outlined"
+                />
+
+                <TextField
+                    style={{marginBottom: 10}}
+                    onChange={(e) => {
+                        setDescription(e.target.value)
+                    }}
+                    fullWidth={true}
+                    label="Description"
+                    variant="outlined"
+                />
+
+                <TextField
+                    style={{marginBottom: 10}}
+                    onChange={(e) => {
+                        setImage(e.target.value)
+                    }}
+                    fullWidth={true}
+                    label="Image link"
+                    variant="outlined"
+                />
+
+                <TextField
+                    style={{marginBottom: 10}}
+                    onChange={(e) => {
+                        setPrice(e.target.value)
+                    }}
+                    fullWidth={true}
+                    label="Price"
+                    variant="outlined"
+                />
+
+                <Button
+                    size={"large"}
+                    variant="contained"
+                    onClick={async () => {
+                        await axios.post(`${BASE_URL}/admin/courses`, {
+                            title: title,
+                                description: description,
+                                imageLink: image,
+                                published: true,
+                                price
+                        }, {
+                            headers: {
+                                "Authorization": "Bearer " + localStorage.getItem("token")
+                            }
+                        });
+                        alert("Added course!");
+                        navigate("/courses")
+                    }}
+                > Add course</Button>
+            </Card>
+        </div>
+    </div>
+}
+
+export default AddCourse;
